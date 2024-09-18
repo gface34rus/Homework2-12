6f0c597aae2dcced6cc3c0fcd3c05e7bf4bfd52e
gface34rus <89259112970@mail.ru>
2024-09-18 14:05:23 +0300

    Initial commit

---
+++ src/test/java/pro/sky/calculatorWithTests2/CalculatorWithTests2ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.calculatorWithTests2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorWithTests2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
